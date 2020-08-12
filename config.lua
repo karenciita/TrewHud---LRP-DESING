86596a3c07fe544e3f7afbe8bf23be49561382f6
karenciita <57659158+karenciita@users.noreply.github.com>
2020-08-12 10:49:24 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,69 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'https://cdn.discordapp.com/attachments/694465247465046016/712097685351694467/p.png'
+
+--https://i.imgur.com/lZSja0e.png
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 240,
+
+	keys = {
+		seatbelt 	= 'Z',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= true,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= false,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+
+	showMinimap			= true,
+
+	showWeapons			= true,	
+}
